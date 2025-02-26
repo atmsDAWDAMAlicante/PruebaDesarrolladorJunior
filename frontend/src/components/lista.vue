f34ebd6b507b635b56b34d6f6d176fbbd319e880
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-26 13:02:25 +0100

    Imposibilidad de comunicar correctamente con eventBus usando Vue3

--- frontend/src/components/lista.vue
+++ frontend/src/components/lista.vue
@@ -8,16 +8,19 @@ export default{
     },
     data(){
         return {
-            listaResultado: "",
+            listaResultado: "", // Recoge el html con los radio buttons
             elemento: "",
-            tareaEnviar: ""
+            tareaEnviar: "",
+            listaTareas:[]
         }
     },
     mounted(){
-        
+        eventBus.actualizarTareas()
+            // Recoge el html con la lista de tareas del archivo JSON 
+           /* eventBus.actualizarTareas();
             this.listaResultado = eventBus.listaDeTareas;
             console.log("MOUNTED")
-            console.log(this.listaResultado)
+            console.log(this.listaResultado)*/
         
             // Llegado a este punto solicito ayuda de la IA para poder utilizar la API fetch
             // para llamar al JSON local mediante Apache para listar las tareas y efectuar
@@ -34,6 +37,23 @@ export default{
                 }
             })*/
         
+        
+        //console.log("2Actualizar tareas");
+        fetch('http://localhost/PruebaDesarrolladorJunior/data/tareas.json')
+        .then((response)=>response.json())
+        .then((data)=>{
+            let resultado = "";
+            this.listaTareas = data;
+            for (let elem of data){
+                resultado += `<input type='radio' name='numeros' @click='eventBus.seleccionarTarea' value='d'/>${elem.descripcion}<br />`
+            }
+            this.listaResultado = resultado;
+            //return this.listaDeTareas;
+        }).catch((error)=>console.log("Hay un error"));
+    
+
+
+
     },
     watch: {
             'eventBus.tareas': function() {
@@ -49,8 +69,18 @@ export default{
 </script>
 
 <template>
-    <div v-html="listaResultado"></div>
-
+    <!--<div v-html="listaResultado"></div>-->
+    <div>
+        <label v-for="tarea of listaTareas" :key="tarea.id">
+      <input 
+        type="radio" 
+        name="numeros" 
+        :value="tarea.id" 
+        @click="eventBus.metodoPrueba" 
+      />
+      {{ tarea.descripcion }}<br>
+      </label>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/eventBus.js
+++ frontend/src/eventBus.js
@@ -15,22 +15,29 @@ const eventBus = reactive({
     // lista (que contiene la lista de tareas, por tanto tendrá que mostrarlas) y 
     // también se tendrá que ejecutar cuando se completen las acciones CRUD para 
     // mostrar en la lista los cambios efectuados.
+    
+
+  
+
     actualizarTareas(){
-        console.log("2Actualizar tareas");
+        //console.log("2Actualizar tareas");
         fetch('http://localhost/PruebaDesarrolladorJunior/data/tareas.json')
         .then((response)=>response.json())
         .then((data)=>{
-            console.log(data)
             let resultado = "";
             for (let elem of data){
-                resultado += `<input type='radio' name='numeros' name=''/>${elem.descripcion}<br />`
+                resultado += `<input type='radio' name='numeros' value='d'/>${elem.descripcion}<br />`
             }
-            console.log(resultado)
+            this.tareas = data;
             this.listaDeTareas = resultado;
             return this.listaDeTareas;
-        })
+        }).catch((error)=>console.log("Hay un error"));
+    },
+    seleccionarTarea(identificador){
+        console.log(`Tarea seleccionada`);
     }
-
 });
+
+
 
 export default eventBus;
