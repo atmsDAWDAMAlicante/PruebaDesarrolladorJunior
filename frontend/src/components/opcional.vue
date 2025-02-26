39d765a47a53a50ffe1cbc7cd876e1e159c05e2a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-26 08:32:41 +0100

    Componente Opcional: contiene información meteorológica

--- frontend/src/components/opcional.vue
+++ frontend/src/components/opcional.vue
@@ -6,22 +6,29 @@ export default{
     },
     data(){
         return {
-            pruebaOpcional: "componenteOpcional"
+            pruebaOpcional: "componenteOpcional",
+            elTiempoHoy: ""
         }
-    },
-    methods:{
-        prueba(){
-            return this.pruebaOpcional
-        }
+    },mounted(){
+        // Esta API devuelve el tiempo pero sólo del día de hoy y mañana, y referencias a provincias
+        fetch('https://www.el-tiempo.net/api/json/v2/home')
+        .then(response=>response.json())
+        .then((data)=>{
+
+            console.log(`${data.provincias[1].CODPROV}`)
+            this.pruebaOpcional = `${data.origen.productor}`
+            this.elTiempoHoy = `${data.today.p[0]}`
+        })
     }
     
 }
 </script>
 
 <template>
- <div>{{pruebaOpcional}}</div>
+ <div><p></p>{{pruebaOpcional}}<p>{{ elTiempoHoy }}</p></div>
 </template>
 
 <style scoped>
     div{background-color: rgb(221, 249, 249);border:1px solid blue;}
+    p{font-size:0.7em;margin-top:10px;}
 </style>
