a78e5f97279f221e7ba268e7e435b9b3201e1d05
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-26 08:12:57 +0100

    Agregada la API para frases aleatorias (se obtiene error)

--- frontend/src/components/frase.vue
+++ frontend/src/components/frase.vue
@@ -11,21 +11,39 @@ export default{
     },
 
     methods:{
-        prueba(){
-            return this.pruebaFrase
+       obtenerFrase(){ 
         }
+    },mounted(){
+        // Método para obtener una frase de una API https://api.quotable.io/random
+        // No permite la conexión directa sólo apuntando a la URL
+        fetch("https://api.allorigins.win/get?url=" + encodeURIComponent("https://api.quotable.io/random"))
+            .then((response)=>{
+                return response.json()
+            }).then((data)=>{
+                console.log(`Esto es lo que se obtiene: ${data}`);
+                // Aunque la URL de la API sí muestra un JSON con frases/autores aleatorios, a la
+                // petición fetch devuelve un error
+                this.pruebaFrase = `${data.content}" - ${data.author}---error: ${data.error} / stack ${data.stack}`;
+            })
     }
     
 }
 </script>
 
 <template>
- <div>{{pruebaFrase}}</div>
- <div class="container">
-
- </div>
+    <div @DOMContentLoaded="obtenerFrase" class="container">
+        Frase del día: {{pruebaFrase}}
+    </div>
+    <h1>Bienvenido a To-Do</h1>
 </template>
 
 <style scoped>
-    div{background-color: rgb(249, 233, 215);border:1px solid orange;}
+    div{
+    background-color: rgb(249, 233, 215);
+    border:1px solid orange;
+    }
+    h1{
+        text-align: center;
+        margin-top: 20px;
+    }
 </style>
